470a9bbc733a211980e3227f753aa4735c26e7ba
Juuso Nykänen <juuso.nykanen@digijoukkue.fi>
2021-08-22 09:01:45 +0300

    start work on problem37

--- problem37.js
+++ problem37.js
@@ -1,59 +1,16 @@
-//Problem 37
+const { generatePrimeNumbers, sumAll } = require("./primesGenerator");
 
-const sumAll = () => {
-  primesArr.push(2);
-  primesArr.push(5);
-  let sum = 0;
-  primesArr.forEach((prime) => (sum += prime));
-  console.log(primesArr.length); // should be 25 if primes less than 100
-  console.log(sum); // should be 1060 if primes less than 100
+const problem37 = () => {
+  const { primesArr, primesObj } = generatePrimeNumbers(100);
+  sumAll(primesArr);
+  return;
+  /*
+  const primesObj = primesArr.reduce();
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {}
+  }
+  */
 };
 
-const primesArr = [3, 7]; // 5 omitted
-let currentSquareRoot = 3;
-
-const integerSquareRoot = (x) => {
-  const result = Math.sqrt(x);
-  const roundedResult = parseInt(result);
-  if (roundedResult > currentSquareRoot) {
-    currentSquareRoot = roundedResult;
-  }
-  if (result === roundedResult) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const testIfPrime = (primeUnderTest) => {
-  const hasSquareRoot = integerSquareRoot(primeUnderTest);
-  if (hasSquareRoot) return false;
-
-  for (const prime of primesArr) {
-    if (prime >= currentSquareRoot) {
-      primesArr.push(primeUnderTest);
-      return true;
-    }
-    const remainder = primeUnderTest % prime;
-    if (remainder === 0) {
-      return false;
-    }
-  }
-};
-
-const generatePrimeNumbers = () => {
-  let skip5 = 2;
-
-  for (let i = 11; i < 100; i += 2) {
-    if (skip5 === 4) {
-      skip5 = 0;
-    } else {
-      skip5++;
-      testIfPrime(i);
-    }
-  }
-  console.log(JSON.stringify(primesArr));
-  sumAll();
-};
-
-generatePrimeNumbers();
+problem37();
