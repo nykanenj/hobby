18048eb590e61fedcb94e48fb77d236fb8bd2328
Juuso Nykänen <juuso.nykanen@digijoukkue.fi>
2021-08-22 09:43:49 +0300

    Working version for numbers under 100

--- problem37.js
+++ problem37.js
@@ -1,16 +1,44 @@
 const { generatePrimeNumbers, sumAll } = require("./primesGenerator");
 
 const problem37 = () => {
-  const { primesArr, primesObj } = generatePrimeNumbers(100);
-  sumAll(primesArr);
-  return;
-  /*
-  const primesObj = primesArr.reduce();
+  const { primesArr, primesObj } = generatePrimeNumbers(10000);
+  //sumAll(primesArr);
 
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {}
+  const primeDigits = [2, 3, 5, 7];
+  const truncPrimes = [];
+
+  const recursive = (trunc, digits) => {
+    if ((digits = 5)) return;
+
+    for (const i of primeDigits) {
+    }
+  };
+
+  const truncTest = (arr) => {
+    let constructedNumber = 0;
+    let multiplier = 1;
+    for (const val of arr) {
+      constructedNumber += val * multiplier;
+      multiplier *= 10;
+    }
+    if (primesObj[constructedNumber]) {
+      console.log(`ConstructedNumber: ${constructedNumber} found as prime`);
+      truncPrimes.push(constructedNumber);
+      //recursive(constructedNumber, 2);
+      return true;
+    } else {
+      console.log(`ConstructedNumber: ${constructedNumber} not prime`);
+      return false;
+    }
+  };
+
+  for (const i of primeDigits) {
+    for (const j of primeDigits) {
+      truncTest([i, j]);
+      truncTest([j, i]);
+    }
   }
-  */
+  console.log(JSON.stringify(truncPrimes));
 };
 
 problem37();
