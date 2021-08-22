7372d0ac9e9dbcb263a6750c46b970f1e907c1fc
Juuso Nykänen <juuso.nykanen@digijoukkue.fi>
2021-08-22 20:13:06 +0300

    recursive approach

--- problem37.js
+++ problem37.js
@@ -1,16 +1,41 @@
 const { generatePrimeNumbers, sumAll } = require("./primesGenerator");
 
 const problem37 = () => {
-  const { primesArr, primesObj } = generatePrimeNumbers(10000);
+  const upperlimit = 10000;
+  const { primesArr, primesObj } = generatePrimeNumbers(upperlimit);
   //sumAll(primesArr);
 
   const primeDigits = [2, 3, 5, 7];
   const truncPrimes = [];
 
-  const recursive = (trunc, digits) => {
-    if ((digits = 5)) return;
+  const addDigitAtStart = (truncNumber, newDigit, digits) => {
+    return newDigit * Math.pow(10, digits) + truncNumber;
+  };
 
-    for (const i of primeDigits) {
+  const addDigitAtEnd = (truncNumber, newDigit, digits) => {
+    return truncNumber * Math.pow(10, digits) + newDigit;
+  };
+
+  const recursive = (truncNumber, digits) => {
+    if (digits >= 5) return;
+    digits++;
+    //console.log(`${truncNumber}`)
+    if (primesObj[truncNumber]) {
+      truncPrimes.push(truncNumber);
+      for (const newDigit of primeDigits) {
+        console.log(
+          `  truncNumber: ${truncNumber} newDigit: ${newDigit} digits: ${digits}`
+        );
+        console.log(
+          `  addDigitAtStart(truncNumber, newDigit, digits): ${addDigitAtStart(
+            truncNumber,
+            newDigit,
+            digits
+          )}`
+        );
+        recursive(addDigitAtStart(truncNumber, newDigit, digits), digits);
+        recursive(addDigitAtEnd(truncNumber, newDigit, digits), digits);
+      }
     }
   };
 
@@ -20,22 +45,16 @@ const problem37 = () => {
     for (const val of arr) {
       constructedNumber += val * multiplier;
       multiplier *= 10;
+      if (!primesObj[constructedNumber]) return false;
     }
-    if (primesObj[constructedNumber]) {
-      console.log(`ConstructedNumber: ${constructedNumber} found as prime`);
-      truncPrimes.push(constructedNumber);
-      //recursive(constructedNumber, 2);
-      return true;
-    } else {
-      console.log(`ConstructedNumber: ${constructedNumber} not prime`);
-      return false;
-    }
+    console.log(`ConstructedNumber: ${constructedNumber} found as prime`);
+    recursive(constructedNumber, 1);
+    return true;
   };
 
   for (const i of primeDigits) {
     for (const j of primeDigits) {
       truncTest([i, j]);
-      truncTest([j, i]);
     }
   }
   console.log(JSON.stringify(truncPrimes));
